20207285f08d8b2407b02d91bac198f2d35e24e0
tertsert <94333556+tertsert@users.noreply.github.com>
2024-04-10 19:42:58 -0500

    Update Contact.h

--- Contact.h
+++ Contact.h
@@ -4,16 +4,19 @@
 #define MAX_CONTACTS 100
 #define NAME_LEN 50
 #define PHONE_LEN 15
+#define EMAIL_LEN 50
 
-// Struct definition for a contact
-struct Contact {
+// Define a type for Contact struct
+typedef struct {
     char name[NAME_LEN];
     char phoneNumber[PHONE_LEN];
-};
+    char email[EMAIL_LEN]; // New field for email address
+} Contact;
 
 // Function declarations
-void addContact(struct Contact contacts[], int *numContacts, char name[], char phoneNumber[]);
-void displayContacts(struct Contact contacts[], int numContacts);
-void sortContacts(struct Contact contacts[], int numContacts);
+void addContact(Contact contacts[], int *numContacts, char name[], char phoneNumber[], char email[]);
+void displayContacts(Contact contacts[], int numContacts);
+void sortContacts(Contact contacts[], int numContacts);
+void deleteContact(Contact contacts[], int *numContacts, int index); // New function for deleting a contact
 
 #endif // CONTACT_H
